127cdd1a287d149afcfc2bb4d047d634d694bf5b
mirel.voicu <mirel.voicu02@gmail.com>
2021-06-24 17:52:17 +0300

    finalised wordcounter

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import operator
 def homepage(request):
     #return  HttpResponse('Hello')
     return render(request,'home.html')
@@ -11,9 +11,26 @@ def homepage(request):
 def count(request):
     text=request.GET['fulltext']
 
-    num=len(text.split(" "))
+    wordlist = text.split()
+
+    num=len(wordlist)
+
+    worddictionary={}
+
+    for word in wordlist:
+        if word in worddictionary:
+
+            worddictionary[word]+=1
+        else:
+
+            worddictionary[word]=1
+
+    sorted_list=sorted(worddictionary.items(),key=operator.itemgetter(1),reverse=True)
+
+    print("THE SORTED LIST IS:", sorted_list)
+
     return render(
                     request,
                     'count.html',
-                    {'text_entered':text,'words_number':num}
+                    {'text_entered':text,'words_number':num,'wordlist':sorted_list}
                 )
